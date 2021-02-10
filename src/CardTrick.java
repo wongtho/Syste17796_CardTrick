f4f1edc2a1b52bd452d91b6f267965b102a13c05
Thomas Wong <wongtho@sheridancollege.ca>
2021-02-10 16:57:25 -0400

    Added find card input parsing with error message.

--- src/CardTrick.java
+++ src/CardTrick.java
@@ -5,8 +5,7 @@
  *  Program: ICE1: Card Trick Main Class
  *  Due Date: February 9, 2021
  */
-import java.util.Scanner;
-import java.util.Random;
+import java.util.*;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -24,13 +23,37 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++) {
             //create card object
             //error: every loop creates new card, erasing prev
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            c.setValue(randNum.nextInt(13) + 1);
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            c.setSuit(Card.SUITS[randNum.nextInt(4)]);
-            
-            System.out.println("Your card is:" + c.getValue() + " of " + c.getSuit());
+            magicHand[i] = new Card();
+            //generate randome number, store using .setValue()
+            magicHand[i].setValue(randNum.nextInt(13) + 1);
+            //generate randome suit, store using .setSuit()
+            magicHand[i].setSuit(Card.SUITS[randNum.nextInt(4)]);
+            //test
+            System.out.println("Your card is:" + magicHand[i].getValue() + " of " + magicHand[i].getSuit());
+        }
+        //create search variables
+        int parseNum;
+        String parseSuit;
+        //match suit using regex
+        String regex = "[hH]eart(s)?|[dD]iamond(s)?|[sS]pade(s)?|[cC]lub(s)?";
+        //ask user to pick a card number
+        System.out.print("Pick a card 'any card' (1 - 13): ");
+        try {
+            parseNum = userInput.nextInt();
+            if(parseNum < 1 || parseNum > 13) {
+                System.err.println("Only numbers from 1 to 13 are accepted.");
+            }
+        }
+        catch(InputMismatchException e) {
+            throw new NoSuchElementException("Only numbers are accepted.");
+        }
+        //ask user to pick a suit
+        System.out.print("Pick a card 'any card' (Hearts, Diamonds, Spades, Clubs): ");
+        try {
+            parseSuit = userInput.next(regex);
+        }
+        catch(NoSuchElementException e) {
+            System.err.println("No suits matching input.");
         }
         
         //insert code to ask the user for Card value and suit, create their card
